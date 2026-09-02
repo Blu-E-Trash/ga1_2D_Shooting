68ecf710e29389ce3a157c467d14277308a33657
Blu_E_Trash <163538035+Blu-E-Trash@users.noreply.github.com>
2026-09-02 14:31:18 +0900

    feat: Bullet이 양쪽에서 0.5초의 공격속도를 가지고 나오도록 하는 기능
    feat: Bullet이 양쪽에서 0.5초의 공격속도를 가지고 나오도록 하는 기능

--- Assets/02.Scripts/Player/PlayerFire.cs
+++ Assets/02.Scripts/Player/PlayerFire.cs
@@ -2,16 +2,30 @@ using UnityEngine;
 
 public class PlayerFire : MonoBehaviour
 {
-    public Transform bulletSpawnPoint;
+    public Transform FirePointL;
+    public Transform FirePointR;
     public GameObject BulletPrefab;
+
+    public float fireRate = 0.5f; // 발사 속도 (초 단위)
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            //Instantiate는 프리펩을 복사해서 게임 오브젝트를 생성하고 씬에 넣어주는 기능
-            GameObject bullet = Instantiate(BulletPrefab);
-            bullet.transform.position = bulletSpawnPoint.position;
+            if(Time.time >= fireRate)
+            {
+                Fire();
+                fireRate = Time.time + 0.5f; // 다음 발사 가능 시간 설정
+            }
+
         }
+    }
+    private void Fire()
+    {            //Instantiate는 프리펩을 복사해서 게임 오브젝트를 생성하고 씬에 넣어주는 기능
+        GameObject bulletL = Instantiate(BulletPrefab);
+        bulletL.transform.position = FirePointL.position;
+
+        GameObject bulletR = Instantiate(BulletPrefab);
+        bulletR.transform.position = FirePointR.position;
     }
 }
