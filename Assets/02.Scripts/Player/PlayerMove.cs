efbe06350d312c97bd74e11160eb60f63660c468
Blu_E_Trash <163538035+Blu-E-Trash@users.noreply.github.com>
2026-09-02 10:35:16 +0900

    Test: 플레이어 이동 왼쪽 입력
    Test: 플레이어 이동 왼쪽 입력

--- Assets/02.Scripts/Player/PlayerMove.cs
+++ Assets/02.Scripts/Player/PlayerMove.cs
@@ -1,14 +1,20 @@
 using UnityEngine;
+using UnityEngine.Animations;
 
 public class PlayerMove : MonoBehaviour
 {
-    private void Start()
-    {
-        
-    }
+    // 목적: 키보드 입력에 따른 플레이어 이동 처리
+    
+
 
     private void Update()
     {
-        
+        // 1, 키보드 입력을 받는다.
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            Debug.Log("왼쪽 방향키를 누르고 있습니다.");
+        }
+        // 2. 키보드 입력에 따라 방향을 구한다.
+        // 3. 방향과 속도에 따라 이동한다.
     }
 }
