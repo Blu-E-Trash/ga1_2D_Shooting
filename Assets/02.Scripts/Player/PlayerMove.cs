1489d40df50a3fabea8de837632b25795a81b696
Blu_E_Trash <163538035+Blu-E-Trash@users.noreply.github.com>
2026-09-02 13:55:57 +0900

    refactor: 메서드 분리
    refactor: 메서드 분리

--- Assets/02.Scripts/Player/PlayerMove.cs
+++ Assets/02.Scripts/Player/PlayerMove.cs
@@ -11,44 +11,51 @@ public class PlayerMove : MonoBehaviour
 
     private void Update()
     {
+        Move();
+        SpeedChange();
+    }
+
+    private void Move()
+    {
         float h = Input.GetAxisRaw("Horizontal"); // 좌우 -1 ~ 1 사이의 값 반환
         float v = Input.GetAxisRaw("Vertical");   // 상하 -1 ~ 1 사이의 값 반환
 
         Vector2 direction = new Vector2(h, v);
 
-
-
-        if (Input.GetKey(KeyCode.E))
-        {
-             Speed += 1f*Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.Q))
-        {
-            if(Speed > 0)
-            {
-                Speed -= 1f*Time.deltaTime;
-            }
-        }
         Vector2 normalizedSpeed = direction.normalized * Speed;
 
         transform.position += (Vector3)(normalizedSpeed * Time.deltaTime);
 
 
-        if(transform.position.x < minPosX)
+        if (transform.position.x < minPosX)
         {
             transform.position = new Vector2(maxPosX, transform.position.y);
         }
-        if(transform.position.x > maxPosX)
+        if (transform.position.x > maxPosX)
         {
             transform.position = new Vector2(minPosX, transform.position.y);
         }
-        if(transform.position.y < minPosY)
+        if (transform.position.y < minPosY)
         {
             transform.position = new Vector2(transform.position.x, minPosY);
         }
-        if(transform.position.y > maxPosY)
+        if (transform.position.y > maxPosY)
         {
             transform.position = new Vector2(transform.position.x, maxPosY);
+        }
+    }
+    private void SpeedChange()
+    {
+        if (Input.GetKey(KeyCode.E))
+        {
+            Speed += 1f * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            if (Speed > 0)
+            {
+                Speed -= 1f * Time.deltaTime;
+            }
         }
     }
 }
