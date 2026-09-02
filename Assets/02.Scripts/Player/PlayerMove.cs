ba2bf1d50a58456e2017418a2b0640760e6515bf
Blu_E_Trash <163538035+Blu-E-Trash@users.noreply.github.com>
2026-09-02 13:37:55 +0900

    Fix: 매직넘버 수정 및 변수명 변경
    Fix: 매직넘버 수정 및 변수명 변경

--- Assets/02.Scripts/Player/PlayerMove.cs
+++ Assets/02.Scripts/Player/PlayerMove.cs
@@ -3,30 +3,19 @@ using UnityEngine.Animations;
 
 public class PlayerMove : MonoBehaviour
 {
-    // 목적: 키보드 입력에 따른 플레이어 이동 처리
+    public float Speed = 5f;
+    private float minPosX = -2.3f;
+    private float maxPosX = 2.3f;
+    private float minPosY = -4.68f;
+    private float maxPosY = 0f;
 
-    public float Speed = 5f; // 이동 속력
-    private float minX = -2.3f;
-    private float maxX = 2.3f;
-    private float minY = -4.68f;
-    private float maxY = 0f;
-
-    // 매 프레임마다 실행
-    // 초당 프레임 실행 횟수는 별다른 설정이 없으면 가능한 만큼 실행
     private void Update()
     {
-
-        // 1, 키보드 입력을 받는다.
         float h = Input.GetAxisRaw("Horizontal"); // 좌우 -1 ~ 1 사이의 값 반환
         float v = Input.GetAxisRaw("Vertical");   // 상하 -1 ~ 1 사이의 값 반환
 
-        // 2. 키보드 입력에 따라 방향을 구한다.
-        // 게임에는 벡터라는 타입이 있음. 벡터는 크기와 방향을 의미함.
-        // 왼쪽 방향을 의미하는 벡터
-        Vector2 direction = new Vector2(h, v); // = Vector2 direction = Vector2.left);
+        Vector2 direction = new Vector2(h, v);
 
-        //Debug.Log($"{h}, {v}");
-        //Debug.Log("왼쪽 방향키를 누르고 있습니다.");
 
 
         if (Input.GetKey(KeyCode.E))
@@ -40,32 +29,26 @@ public class PlayerMove : MonoBehaviour
                 Speed -= 1f*Time.deltaTime;
             }
         }
-
-        // 3. 방향과 속도에 따라 이동한다.
-        // 속도 = 방향 * 속력          //매직넘버 : 보는 사람에 따라 의미가 달라질 수 있는 헷갈리는 숫자    
-        //transform.Translate(direction * Speed * Time.deltaTime);
-        // deltaTime : 이전 프레임이 끝나고 다음 프레임이 시작될 때까지 걸린 시간을 Ms로 반환
         Vector2 normalizedSpeed = direction.normalized * Speed;
 
-        // 새로운 위치 = 현재 위치 + (방향 * 속력 * 시간)
         transform.position += (Vector3)(normalizedSpeed * Time.deltaTime);
 
 
-        if(transform.position.x < minX)
+        if(transform.position.x < minPosX)
         {
-            transform.position = new Vector2(maxX, transform.position.y);
+            transform.position = new Vector2(maxPosX, transform.position.y);
         }
-        if(transform.position.x > maxX)
+        if(transform.position.x > maxPosX)
         {
-            transform.position = new Vector2(minX, transform.position.y);
+            transform.position = new Vector2(minPosX, transform.position.y);
         }
-        if(transform.position.y < minY)
+        if(transform.position.y < minPosY)
         {
-            transform.position = new Vector2(transform.position.x, minY);
+            transform.position = new Vector2(transform.position.x, minPosY);
         }
-        if(transform.position.y > maxY)
+        if(transform.position.y > maxPosY)
         {
-            transform.position = new Vector2(transform.position.x, maxY);
+            transform.position = new Vector2(transform.position.x, maxPosY);
         }
     }
 }
