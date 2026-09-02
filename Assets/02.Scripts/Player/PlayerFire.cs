15df833e937160ad85e6bed2e641c43aea63bf5c
Blu_E_Trash <163538035+Blu-E-Trash@users.noreply.github.com>
2026-09-02 14:36:11 +0900

    feat: 자동 공격 기능 및 자동/수동 모드 전환 기능 추가

--- Assets/02.Scripts/Player/PlayerFire.cs
+++ Assets/02.Scripts/Player/PlayerFire.cs
@@ -5,17 +5,31 @@ public class PlayerFire : MonoBehaviour
     public Transform FirePointL;
     public Transform FirePointR;
     public GameObject BulletPrefab;
+    public bool isAutoFire = false;
 
     public float fireRate = 0.5f; // 발사 속도 (초 단위)
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            isAutoFire = !isAutoFire;
+        }
+
+        if (isAutoFire)
+        {
+            if (Time.time >= fireRate)
+            {
+                Fire();
+                fireRate = Time.time + 0.5f;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.Space))
         {
             if(Time.time >= fireRate)
             {
                 Fire();
-                fireRate = Time.time + 0.5f; // 다음 발사 가능 시간 설정
+                fireRate = Time.time + 0.5f;
             }
 
         }
