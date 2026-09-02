22a82fd4541aad0114c29381b91698a89cada6f2
Blu_E_Trash <163538035+Blu-E-Trash@users.noreply.github.com>
2026-09-02 11:06:10 +0900

    불필요한 Debug.Log 제거
    불필요한 Debug.Log 제거

--- Assets/02.Scripts/Player/PlayerMove.cs
+++ Assets/02.Scripts/Player/PlayerMove.cs
@@ -23,8 +23,8 @@ public class PlayerMove : MonoBehaviour
         // 왼쪽 방향을 의미하는 벡터
         Vector2 direction = new Vector2(h, v); // = Vector2 direction = Vector2.left);
 
-        Debug.Log($"{h}, {v}");
-        Debug.Log("왼쪽 방향키를 누르고 있습니다.");
+        //Debug.Log($"{h}, {v}");
+        //Debug.Log("왼쪽 방향키를 누르고 있습니다.");
         // 3. 방향과 속도에 따라 이동한다.
         // 속도 = 방향 * 속력          //매직넘버 : 보는 사람에 따라 의미가 달라질 수 있는 헷갈리는 숫자    
         //transform.Translate(direction * Speed * Time.deltaTime);
