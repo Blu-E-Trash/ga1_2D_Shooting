1b1114b26856541e7a3f58a6e4623710d1161090
Blu_E_Trash <163538035+Blu-E-Trash@users.noreply.github.com>
2026-09-03 15:42:34 +0900

    refactor: 플레이어를 향해 일직선으로 오는 몹 로직 수정
    refactor: 플레이어를 향해 일직선으로 오는 몹 로직 수정

--- Assets/02.Scripts/Enemy/EnemyTypeDownToPlayer.cs
+++ Assets/02.Scripts/Enemy/EnemyTypeDownToPlayer.cs
@@ -1,9 +1,24 @@
 using UnityEngine;
+
 public class EnemyTypeDownToPlayer : Enemy
 {
-    private float _startPositionX = 0f;
+    private Vector2 _moveDirection;
+
     private void Start()
     {
-        this.transform.position = new Vector2(_startPositionX, Random.Range(5.5f, 8.5f));
+        GameObject player = FindAnyObjectByType<PlayerMove>().gameObject;
+
+        _moveDirection = (player.transform.position - this.transform.position).normalized;
+    }
+
+    override protected void Move()
+    {
+        Vector2 normalizedSpeed = _moveDirection * Speed;
+        transform.position += (Vector3)(normalizedSpeed * Time.deltaTime);
+
+        if (transform.position.y < _minPosY)
+        {
+            Destroy(gameObject);
+        }
     }
 }
