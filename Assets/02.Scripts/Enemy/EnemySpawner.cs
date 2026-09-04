54991191d46f0b6c61ec91febdd9a4fc0c8a1594
Blu_E_Trash <163538035+Blu-E-Trash@users.noreply.github.com>
2026-09-04 12:44:19 +0900

    feat: EnemySpawner 구현
    feat: EnemySpawner 구현

---
+++ Assets/02.Scripts/Enemy/EnemySpawner.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    // 일정 시간마다 적을 생성하는 스크립트
+    [Header("적 프리팹")]
+    [SerializeField]
+    private GameObject[] _enemyPrefab;
+    [Header("적 생성 간격")]
+    private float _spawnInterval = 2.0f; // 적 생성 간격 (초 단위)
+    private float _timer;
+
+    [Header("적 생성 위치")]
+    [SerializeField]
+    private Transform[] _spawnPoints; // 적 생성 위치
+
+    private void Start()
+    {
+        InvokeRepeating("SpawnEnemy", _spawnInterval, _spawnInterval);
+    }
+    private void Update()
+    {
+        _timer += Time.deltaTime;
+        if (_timer >= _spawnInterval)
+        {
+            SpawnEnemy();
+
+            _timer = 0f;
+        }
+    }
+
+    private void SpawnEnemy()
+    {
+        int randomIndex = Random.Range(0, _spawnPoints.Length);
+        int randomEnemyIndex = Random.Range(0, _enemyPrefab.Length);
+        Instantiate(_enemyPrefab[randomEnemyIndex], _spawnPoints[randomIndex].position, Quaternion.identity);
+    }
+}
