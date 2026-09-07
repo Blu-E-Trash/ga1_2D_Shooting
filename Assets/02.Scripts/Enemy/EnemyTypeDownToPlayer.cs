f557bb4a60c7ed2d451d6db9cf575110f00cf134
Blu_E_Trash <163538035+Blu-E-Trash@users.noreply.github.com>
2026-09-07 16:28:29 +0900

    feat: 적 오브젝트 회전 기능 추가
    feat: 적 오브젝트 회전 기능 추가

--- Assets/02.Scripts/Enemy/EnemyTypeDownToPlayer.cs
+++ Assets/02.Scripts/Enemy/EnemyTypeDownToPlayer.cs
@@ -8,12 +8,22 @@ public class EnemyTypeDownToPlayer : Enemy
     {
         GameObject player = FindAnyObjectByType<PlayerMove>().gameObject;
 
-        _moveDirection = (player.transform.position - this.transform.position).normalized;
+        Vector2 direction = player.transform.position - this.transform.position;
+        _moveDirection = direction.normalized;
+
+        RotateTowards(direction);
     }
 
     override protected void Move()
     {
         Vector2 normalizedSpeed = _moveDirection * Speed;
         transform.position += (Vector3)(normalizedSpeed * Time.deltaTime);
+    }
+
+    private void RotateTowards(Vector2 direction)
+    {
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        transform.rotation = Quaternion.Euler(0, 0, angle + 90f);
     }
 }
--- Assets/02.Scripts/Enemy/EnemyTypeFollowPlayer.cs
+++ Assets/02.Scripts/Enemy/EnemyTypeFollowPlayer.cs
@@ -3,19 +3,22 @@ using UnityEngine;
 public class EnemyTypeFollowPlayer : Enemy
 {
     public GameObject Player;
+
     private void Start()
     {
         Player = FindAnyObjectByType<PlayerMove>().gameObject;
     }
-    private void Update()
-    {
-        Move();
-    }
 
     override protected void Move()
     {
-        Vector2 direction = Player.transform.position - transform.position;
-        Vector2 normalizedSpeed = direction.normalized * Speed;
-        transform.position += (Vector3)(normalizedSpeed * Time.deltaTime);
+        if (Player != null)
+        {
+            Vector2 direction = Player.transform.position - transform.position;
+            Vector2 normalizedSpeed = direction.normalized * Speed;
+            transform.position += (Vector3)(normalizedSpeed * Time.deltaTime);
+
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0, 0, angle + 90f);
+        }
     }
 }
