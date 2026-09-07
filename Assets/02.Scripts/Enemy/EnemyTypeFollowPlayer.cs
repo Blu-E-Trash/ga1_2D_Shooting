ae2612d056bc264081875566dd7da09abafc8364
Blu_E_Trash <163538035+Blu-E-Trash@users.noreply.github.com>
2026-09-07 16:32:08 +0900

    refactor: 이동 및 회전 로직 분리
    refactor: 이동 및 회전 로직 분리

--- Assets/02.Scripts/Enemy/EnemyTypeFollowPlayer.cs
+++ Assets/02.Scripts/Enemy/EnemyTypeFollowPlayer.cs
@@ -17,8 +17,12 @@ public class EnemyTypeFollowPlayer : Enemy
             Vector2 normalizedSpeed = direction.normalized * Speed;
             transform.position += (Vector3)(normalizedSpeed * Time.deltaTime);
 
-            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.Euler(0, 0, angle + 90f);
+            Rotate(direction);
         }
+    }
+    private void Rotate(Vector2 direction)
+    {
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, angle + 90f);
     }
 }
