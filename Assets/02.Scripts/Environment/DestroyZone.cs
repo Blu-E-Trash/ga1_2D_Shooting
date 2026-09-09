8fd2a629855ceda8e969b433fef6764b08de0213
Blu_E_Trash <163538035+Blu-E-Trash@users.noreply.github.com>
2026-09-09 11:08:11 +0900

    fix: DestroyZone위치 수정
    fix: DestroyZone위치 수정

--- Assets/02.Scripts/Environment/DestroyZone.cs
+++ Assets/02.Scripts/Environment/DestroyZone.cs
@@ -4,6 +4,13 @@ public class DestroyZone : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        if (collision != null)
+        {
+            if (collision.gameObject.name == "BossEnemy")
+            {
+                return;
+            }
+        }
         Destroy(collision.gameObject);
     }
 }
