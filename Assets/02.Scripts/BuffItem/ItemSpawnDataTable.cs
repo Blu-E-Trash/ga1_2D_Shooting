64efbc242d7a6dc06f0716c731db4c4d8a58446c
Blu_E_Trash <163538035+Blu-E-Trash@users.noreply.github.com>
2026-09-11 11:14:19 +0900

    fix: ItemSpawnDataTabled의 Inspector 메뉴 이름의 불일치 해결
    fix: ItemSpawnDataTabled의 Inspector 메뉴 이름의 불일치 해결

--- Assets/02.Scripts/BuffItem/ItemSpawnDataTable.cs
+++ Assets/02.Scripts/BuffItem/ItemSpawnDataTable.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "ItemSpawnDataTable", menuName = "Scriptable Objects/ItemSpawnDataTableSO")]
+[CreateAssetMenu(fileName = "ItemSpawnDataTable", menuName = "Scriptable Objects/ItemSpawnDataTable")]
 public class ItemSpawnDataTable : ScriptableObject
 {
     public ItemSpawnData[] Datas;
