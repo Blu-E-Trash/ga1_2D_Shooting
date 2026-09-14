1af4dd8544fa61dce0e4a8028e3eeb19b0f132b3
Blu_E_Trash <163538035+Blu-E-Trash@users.noreply.github.com>
2026-09-14 10:42:18 +0900

    feat: 버튼 효과음 및 크기 변경 애니메이션 추가
    feat: 버튼 효과음 및 크기 변경 애니메이션 추가

--- Assets/02.Scripts/UI/UI_AutoButton.cs
+++ Assets/02.Scripts/UI/UI_AutoButton.cs
@@ -1,15 +1,25 @@
 using UnityEngine;
+using UnityEngine.EventSystems; // 터치 이벤트를 사용하기 위해 추가
 using UnityEngine.UI;
 
-public class UI_AutoButton : MonoBehaviour
+public class UI_AutoButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
     [SerializeField]
     private Image _autoImage;
     [SerializeField]
     private Sprite[] _image;
+
+    [SerializeField]
+    private AudioClip _clickSound;
+    [SerializeField]
+    private float _pressedScale = 0.9f;
+
+    private Vector3 _originalScale;
+
     private bool _autoOn = false;
     private GameObject _player;
     private PlayerFire _playerFire;
+
     private void Start()
     {
         _player = GameObject.FindGameObjectWithTag("Player");
@@ -19,15 +29,39 @@ public class UI_AutoButton : MonoBehaviour
             _playerFire = _player.GetComponent<PlayerFire>();
         }
         InitImage();
+
+        _originalScale = transform.localScale;
     }
+
     private void InitImage()
     {
         _autoImage.sprite = _autoOn ? _image[1] : _image[0];
     }
+
     public void ToggleAuto()
     {
         _autoOn = !_autoOn;
         InitImage();
-        _playerFire.SetAuto(_autoOn);
+
+        if (_playerFire != null)
+        {
+            _playerFire.SetAuto(_autoOn);
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        transform.localScale = _originalScale * _pressedScale;
+
+        if (_clickSound != null)
+        {
+            Vector3 playPos = Camera.main != null ? Camera.main.transform.position : transform.position;
+            AudioSource.PlayClipAtPoint(_clickSound, playPos);
+        }
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        transform.localScale = _originalScale;
     }
 }
